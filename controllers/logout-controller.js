49f269bbb5a68a253b785c41d09b8e8e00a83882
Aniket <aniket510719068@gmail.com>
2024-09-24 13:44:50 +0530

    adfjasdf

--- controllers/logout-controller.js
+++ controllers/logout-controller.js
@@ -1,6 +1,9 @@
-
-
-export async function logoutController (req, res){
-    res.cookie("token", "", { expires: new Date(0), httpOnly: true });
-    res.status(200).send({ message: "Logged out successfully" });
-  }
+export async function logoutController(req, res) {
+  res.cookie("token", "", {
+    expires: new Date(),
+    httpOnly: true,
+    secure: true, // Ensure cookies are sent over HTTPS
+    sameSite: "None", // Enable cross-origin requests to store cookies
+  });
+  res.status(200).send({ message: "Logged out successfully" });
+}
