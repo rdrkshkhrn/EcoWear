deae818651b889964cf45db5523348cc91a55754
Aniket <aniket510719068@gmail.com>
2025-02-17 21:20:40 +0530

    loading added

--- controllers/signin-controller.js
+++ controllers/signin-controller.js
@@ -5,7 +5,7 @@ import bcrypt from "bcryptjs";
 export  async function signinController (req, res)  {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-  
+    
     
     if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(401).json({ message: "Invalid email or password" });
@@ -16,5 +16,6 @@ export  async function signinController (req, res)  {
         secure: true, // Ensure cookies are sent over HTTPS
         sameSite: 'None' // Enable cross-origin requests to store cookies
         });
-    res.status(200).json({ message: "Sign-in successful" });
+    res.status(200).json({ message: "Sign-in successful" }
+    );
   }
--- utility/initialize-mongodb.js
+++ utility/initialize-mongodb.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 export async function initializeMongoDb(){
     try{
         const uri = process.env.MONGODB_URI;
-        // const client = new MongoClient(uri);
-        // await client.connect();
         const mongooseClient = await mongoose.connect(uri);
         console.log("mongo db intialized!");
         return mongooseClient.connection;
